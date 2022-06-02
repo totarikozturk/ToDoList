d72e92ae8566581b59eba131d9d045f3f486790f
Tarik <tarkztrk@icloud.com>
2022-06-02 20:53:56 +0300

    updated gitignore

--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -6,11 +6,22 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    lazy var box = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.addSubview(box)
+        box.backgroundColor = .green
+        box.snp.makeConstraints {
+            (make) -> Void in
+            make.width.height.equalTo(50)
+            make.center.equalTo(self.view)
+        }
     }
 
 
